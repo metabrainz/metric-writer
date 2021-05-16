cb582c7a90f2a2c711d9f60704902c2f9d3874a3
Robert Kaye <rob@musicbrainz.org>
2021-05-16 14:05:05 +0000

    Various fixes

--- metric_writer.py
+++ metric_writer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
-from time import sleep
+from time import sleep, monotonic
 import sys
 
 import redis
@@ -11,7 +11,8 @@ import requests
 from brainzutils.metrics import REDIS_METRICS_KEY
 import config
 
-SERVCE_CHECK_INTERVAL = 5  #60  # seconds
+SERVCE_CHECK_INTERVAL = 15  # seconds
+REPORT_INTERVAL = SERVCE_CHECK_INTERVAL * 40 
 
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
@@ -25,43 +26,37 @@ def process_redis_server(redis_server, redis_port, redis_namespace):
         the metric and carry on.
     """
 
-    log.info("check redis %s" % redis_server)
     r = redis.Redis(host=redis_server, port=redis_port)
 
     lines = ""
     count = 0
     while True:
         line = r.lpop(REDIS_METRICS_KEY)
-        log.info(line)
         if not line:
             break
 
-        line = str(line, "utf-8")
-        log.info("got data: '%s'" % str(line))
-
-        lines += line + "\n"
+        lines += str(line, "utf-8")
         count += 1
 
     if not lines:
-        return
-
-    log.info("Collected %d lines of points" % count)
+        return 0
 
     for tries in range(5):
         params = { "p" : "root", "db": "service-metrics", "u": "root" }
         r = requests.post("http://%s:%d/write" % (config.INFLUX_SERVER, config.INFLUX_PORT), params=params, data=lines)
         if r.status_code in (200, 204):
-            log.error("submitted data point.");
-            break
+            return count
 
         if str(r.status_code)[0] == '4':
             log.error("Cannot write metric due to 4xx error. %s" % r.text)
-            break
+            return 0
 
         log.warning("Cannot write metric due to other error Retyring. %s" % r.text)
         sleep(3)
     else:
         log.error("Data points not submitted due to repeated submission problems.")
+
+    return 0
 
 
 def main():
@@ -71,11 +66,16 @@ def main():
         sentry_sdk.init(**config.LOG_SENTRY)
 
     log.info("metric writer starting!")
+    count = 0
+    update_time = monotonic() + REPORT_INTERVAL
     while True:
         for info in config.redis_servers: 
-            process_redis_server(info["host"], info["port"], info["namespace"])
+            count += process_redis_server(info["host"], info["port"], info["namespace"])
 
         sleep(SERVCE_CHECK_INTERVAL)
+        if monotonic() > update_time:
+            update_time += REPORT_INTERVAL
+            log.info("Submitted %s data points" % count)
 
 
 if __name__ == "__main__":
